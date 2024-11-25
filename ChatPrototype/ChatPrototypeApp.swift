b8a9b5733e4d21abb8624afd32c9709a18b6424c
Syed Zia <muhammadhasaansyed@icloud.com>
2024-11-25 12:37:03 +0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Syed Zia ur Rehman on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
